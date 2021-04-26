5129c6d9616ddf363ed9878bc749064d31d0be89
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-04-26 14:38:22 +0700

    alert

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link :to="{name:'home'}">Home</router-link> |
-
+    <router-link :to="{name:'alert'}">Alert</router-link>
   </div>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import alert from '../views/alert/alert.vue'
 
 const routes = [
   {
@@ -9,7 +9,11 @@ const routes = [
     component: Home
   },
 
- 
+  {
+    path: '/alert',
+    name: 'alert',
+    component: alert
+  },
 
 ]
 
