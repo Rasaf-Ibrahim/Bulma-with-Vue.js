2fd46a5f849de7ad10824b2e38c11b92aca36831
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-08-14 00:56:04 +0600

    1.7

--- components/CodeLink.vue
+++ components/CodeLink.vue
@@ -1,39 +1,36 @@
 <template>
-
-
-<div class="bg-s-t1a2 shadow 
-     max-w-90% md:max-w-75% m-auto text-center p-4
-     rounded-xl">
-
+  <div
+    class="
+      bg-s-t1a2
+      shadow
+      max-w-90%
+      md:max-w-75%
+      m-auto
+      text-center
+      p-4
+      rounded-xl
+    "
+  >
     <div class="-p">
-        Check out the code of
-    
-        <a :href="link" 
-            class="font-bold text-b-t1a2
-            hover:text-b-t1a3
-            cursor-pointer"
-            title="Click">
+      Check out the code of
 
-            {{name}} 
-        </a>
-        Component.
-       
+      <a
+        :href="link"
+        class="font-bold text-b-t1l1a2 hover:text-b-t1l1a3 cursor-pointer"
+        title="Click"
+      >
+        {{ name }}
+      </a>
+      Component.
     </div>
-
-</div>
-
-
-
-
+  </div>
 </template>
 
 
 <script>
-    export default {
-
-        props: ['link', 'name']
-        
-    }
+export default {
+  props: ["link", "name"],
+};
 </script>
 
 
--- components/card/imageCard/child/Cards.vue
+++ components/card/imageCard/child/Cards.vue
@@ -8,26 +8,27 @@
 
         
         <Card
-            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/src/assets/image/tourist-destination/ratargul.jpg"
+            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/assets/image/tourist-destination/ratargul.jpg"
             caption="Ratargul Swamp Forest"
             siteLink="https://en.wikipedia.org/wiki/Ratargul_Swamp_Forest"
         />
 
 
+
         <Card
-            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/src/assets/image/tourist-destination/jaflong.jpg"
+            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/assets/image/tourist-destination/jaflong.jpg"
             caption="Jaflong"
             siteLink="https://en.wikipedia.org/wiki/Jaflong"
         />
 
          <Card
-            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/src/assets/image/tourist-destination/tanguar.jpg"
+            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/assets/image/tourist-destination/tanguar.jpg"
             caption="Tanguar Haor"
             siteLink="https://en.wikipedia.org/wiki/Tanguar_Haor"
         />
 
         <Card
-            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/src/assets/image/tourist-destination/rema-kalenga.jpg"
+            fileLink="https://raw.githubusercontent.com/Rasaf-Ibrahim/Some-Designed-Components/master/assets/image/tourist-destination/rema-kalenga.jpg"
             caption="Rema-Kalenga Wildlife Sanctuary"
             siteLink="https://en.wikipedia.org/wiki/Rema-Kalenga_Wildlife_Sanctuary"
         />
